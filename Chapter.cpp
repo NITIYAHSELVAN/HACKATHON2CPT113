1d3f2c8f55046abf5c15af1ad9ee152bab0f1fb0
NITIYAHSELVAN <137810539+NITIYAHSELVAN@users.noreply.github.com>
2023-06-28 13:56:52 +0800

    Create Chapter.cpp

---
+++ Chapter.cpp
@@ -0,0 +1,45 @@
+%%writefile Chapter.cpp
+#include <iostream>
+#include <string>
+#include "Chapter.h"
+
+void Chapter :: chapterDisplay1 () {
+
+  cout<< "Lost in the jungle during a trekking expedition with friends, \n";
+  cout<< "I realized there was no signal on my phone to call for help.\n";
+  cout<< "With no other option but to search for my friends, \n";
+  cout<< "I quickly gathered essential items in my bag.\n";
+  cout<< "Given the limited space (max. 3 items), I had to prioritize the most crucial items for survival.\n";
+
+}
+
+void Chapter :: chapterDisplay2 (){
+  cout<< "You continued walking, walking and walking...\n";
+  cout<< "Suddenly, a lion jumped over you!!!\n";
+  cout<< "Somehow, you manage to escape yourself and started running\n";
+  cout<< "You used the rope to climb a very tall tree and escaped.";
+  cout<< "The rope is damaged and left behind\n";
+
+}
+
+void Chapter :: chapterDisplay3 (){
+    cout<< "You shot the bear and left it injured\n";
+}
+
+void Chapter :: chapterDisplay4 (){
+
+}
+
+void Chapter :: chapterDisplay5 (){
+
+}
+
+void Chapter :: chapterDisplay6 (){
+
+};
+
+void Chapter :: chapterDisplay7 (){
+
+};
+
+
