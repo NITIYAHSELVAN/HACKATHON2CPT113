38ef7adfb517db334a36e1c89c0f378989796fc5
NITIYAHSELVAN <137810539+NITIYAHSELVAN@users.noreply.github.com>
2023-06-28 13:58:43 +0800

    Create Scene.cpp

---
+++ Scene.cpp
@@ -0,0 +1,16 @@
+%%writefile 
+#include <iostream>
+#include <string>
+#include "Scene.h"
+
+void Scene :: sceneDisplay1 () {
+  cout<< "save from tiger but it is chasing\n";
+}
+
+void Scene :: sceneDisplay2 (){
+  cout<< "You escaped from the tiger\n";
+}
+
+void Scene :: sceneDisplay3 (){
+
+}
