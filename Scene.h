8f706c2a8541f14dd478ee515c6abd81678f8d20
NITIYAHSELVAN <137810539+NITIYAHSELVAN@users.noreply.github.com>
2023-06-28 13:58:26 +0800

    Create Scene.h

---
+++ Scene.h
@@ -0,0 +1,16 @@
+%%writefile Scene.h
+#include "Intro.h"
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class Scene {
+public:
+void sceneDisplay1 ();
+void sceneDisplay2 ();
+void sceneDisplay3 ();
+void sceneDisplay4 ();
+void sceneDisplay5 ();
+void sceneDisplay6 ();
+void sceneDisplay7 ();
+};
