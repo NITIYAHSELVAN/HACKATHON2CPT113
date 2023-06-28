e1ad941c1c5fa0613662434467357a2b12169fd9
NITIYAHSELVAN <137810539+NITIYAHSELVAN@users.noreply.github.com>
2023-06-28 13:57:35 +0800

    Create Choice.h

---
+++ Choice.h
@@ -0,0 +1,19 @@
+%%writefile Choice.h
+#include "Intro.h"
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class Choice {
+public:
+void choiceDisplay1 ();
+void choiceDisplay2 ();
+void choiceDisplay3 ();
+void choiceDisplay4 ();
+void choiceDisplay5 ();
+void choiceDisplay6 ();
+void choiceDisplay6_2 ();
+void choiceDisplay7 ();
+void choiceDisplay8 ();
+void choiceDisplay9 ();
+};
