d76a12f104711d358681a7c45331845944afb2dc
NITIYAHSELVAN <137810539+NITIYAHSELVAN@users.noreply.github.com>
2023-06-28 13:56:31 +0800

    Create Chapter.h

---
+++ Chapter.h
@@ -0,0 +1,16 @@
+%%writefile Chapter.h
+#include "Intro.h"
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class Chapter {
+public:
+void chapterDisplay1 ();
+void chapterDisplay2 ();
+void chapterDisplay3 ();
+void chapterDisplay4 ();
+void chapterDisplay5 ();
+void chapterDisplay6 ();
+void chapterDisplay7 ();
+};
