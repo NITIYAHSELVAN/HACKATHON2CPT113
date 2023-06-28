c3ce36b832ff120dec3044ba2421f8621976c99e
NITIYAHSELVAN <137810539+NITIYAHSELVAN@users.noreply.github.com>
2023-06-28 13:53:55 +0800

    Create Intro.h

---
+++ Intro.h
@@ -0,0 +1,20 @@
+# Intro.h
+
+%%writefile Intro.h
+#ifndef INTRO_H
+#define INTRO_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Intro
+{
+
+  public:
+    int startInput;
+    void introDisplay();
+    void gameOver();
+
+};
+
+#endif
